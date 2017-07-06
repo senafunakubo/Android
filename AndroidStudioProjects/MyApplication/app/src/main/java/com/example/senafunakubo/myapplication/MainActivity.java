0c653214d4660f5102941c2536c518c8e6583432
Sena <inabacker24@gmail.com>
2017-07-05 22:35:11 -0700

    start android class

---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/senafunakubo/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.senafunakubo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
+
+
+public class MainActivity extends AppCompatActivity {
+        String tag = "LifeCycleEvents";
+        /** Called when the activity is first created. */
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.d(tag, "In the onCreate() event");
+        }
+        public void onStart()
+        {
+            super.onStart();
+            Log.d(tag, "In the onStart() event");
+        }
+        public void onRestart()
+        {
+            super.onRestart();
+            Log.d(tag, "In the onRestart() event");
+        }
+        public void onResume()
+        {
+            super.onResume();
+            Log.d(tag, "In the onResume() event");
+        }
+        public void onPause()
+        {
+            super.onPause();
+            Log.d(tag, "In the onPause() event");
+        }
+        public void onStop()
+        {
+            super.onStop();
+            Log.d(tag, "In the onStop() event");
+        }
+        public void onDestroy()
+        {
+            super.onDestroy();
+            Log.d(tag, "In the onDestroy() event");
+        }
+}
