fe8d1dea3a9a88b4d6aa1bf9fd2fd4666e0894db
Sena <inabacker24@gmail.com>
2017-07-06 01:20:29 -0700

    Add assignments

---
+++ AndroidStudioProjects/Assignment1-1/app/src/main/java/com/example/senafunakubo/assignment1_1/LinearLayoutAssignment.java
@@ -0,0 +1,13 @@
+package com.example.senafunakubo.assignment1_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutAssignment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_assignment);
+    }
+}
